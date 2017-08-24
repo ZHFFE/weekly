75365567daae42dfe428437297fef43e54c58d54
刘卿 <lqing@jointwisdom.cn>
2017-08-24 14:44:11 +0800

    样式优化

--- src/components/summary.vue
+++ src/components/summary.vue
@@ -1,11 +1,18 @@
 <style scoped lang="less">
   @import "../assets/css/home.less";
+  .week-list{
+    font-size: 20px;
+  }
+  .week-list li{
+    float: left;
+    margin: 10px;
+  }
 </style>
 
 <template>
-  <ul>
+  <ul class="week-list">
     <li v-for="item in values">
-      <router-link :to="{path: 'week', query: {week: item.week, year: item.year}}">第{{item.pk}}期</router-link>
+      <router-link :to="{path: 'week', query: {week: item.week, year: item.year}}">第{{item.pk | week}}期</router-link>
     </li>
   </ul>
 </template>
@@ -19,6 +26,12 @@
         values: []
       }
     },
+  filters: {
+    week: function (value) {
+      value = parseInt(value) + 1;
+      return value
+    }
+  },
     methods: {
       getWeeks(){
         this.$http.get('/weeks', {
